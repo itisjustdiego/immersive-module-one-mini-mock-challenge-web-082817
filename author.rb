42430dbcbbef304b1bc8ef6a211dd09fd70ba55f
itisjustdiego <itisjustdiego@gmail.com>
2017-09-08 11:13:33 -0400

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,38 @@
+class Author
 
+  attr_accessor :author
+  @@authors = []
+
+  def initialize(author)
+    @author = author,
+    @@authors << self.author
+  end
+
+  def self.all
+    @@authors
+  end
+
+  def books
+    #return books author has written
+    #access book class match books
+    #with authors, output data
+  end
+
+  def write_book(title, word_count)
+    #create new book instance
+    #associate with author
+    #.new
+  end
+
+  def total_words
+    #return total num of words for
+    #author across all books
+    #find where author matches books
+    #tally up word_count
+  end
+
+  def self.most_words
+  end
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,28 @@
+class Book
+  attr_accessor :author, :title, :word_count
+  @@books = []
 
+  def initialize(author, title, word_count)
+    @author = author,
+    @title = title,
+    @word_count = word_count,
+    @@books << self.title
+  end
+
+  def self.all
+    @@books
+  end
+
+  #
+  # def self.author
+  #  @ author
+  # end
+  #
+  # def self.title
+  #   @title
+  # end
+  #
+  # def self.word_count
+  #   @word_count
+  # end
+end
